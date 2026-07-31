ea4f751e265453cd18a2fbdd2945ae1f884db7ed
calbee4 <cjbarge123@gmail.com>
2026-07-31 03:06:30 -0400

    Setting up base for customer class

--- FinalPart1/FinalPart1.py
+++ FinalPart1/FinalPart1.py
@@ -7,6 +7,39 @@
 # Class Area
 # --------------------
 
+class Customer:
+    def __init__(self, strName, intID):
+        self.strName = strName
+        self.intID = intID
+
+    @property
+    def strName(self):
+        return self.__strName
+
+    @property
+    def intID(self):
+        return self.__intID
+
+    @strName.setter
+    def strName(self, strValue):
+        if strValue == "":
+            raise Exception("strName should not be empty")
+        else:
+            self.__strName = strValue
+
+    @intID.setter
+    def intID(self, intValue):
+        try:
+            intValue = int(intValue)
+
+            if intValue < 0:
+                raise Exception("intID should be greater than 0")
+            else:
+                self.__intID = intValue
+        except ValueError:
+            raise Exception("intID should be a number")
+
+
 # --------------------
 # Function Area
 # --------------------
