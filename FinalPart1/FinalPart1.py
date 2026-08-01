d41d34c48dec6ec8b6d39d12cbfea331ff4c54be
calbee4 <cjbarge123@gmail.com>
2026-08-01 06:04:46 -0400

    Added rental properties and setters to customer

--- FinalPart1/FinalPart1.py
+++ FinalPart1/FinalPart1.py
@@ -3,14 +3,20 @@
 # Assignment: Final Part 1
 # ------------------------------
 
+from datetime import datetime, timedelta
+from typing import Self
+
 # --------------------
 # Class Area
 # --------------------
 
 class Customer:
-    def __init__(self, strName, intID):
+    def __init__(self, strName, intID, strRentalBasis = None, fltRentalTime = None, intRentalQuantity = None):
         self.strName = strName
         self.intID = intID
+        self.__strRentalBasis = strRentalBasis
+        self.__fltRentalTime = fltRentalTime
+        self.__intRentalQuantity = intRentalQuantity
 
     @property
     def strName(self):
@@ -19,6 +25,18 @@ class Customer:
     @property
     def intID(self):
         return self.__intID
+
+    @property
+    def strRentalBasis(self):
+        return self.__strRentalBasis
+
+    @property
+    def fltRentalTime(self):
+        return self.__fltRentalTime
+
+    @property
+    def intRentalQuantity(self):
+        return self.__intRentalQuantity
 
     @strName.setter
     def strName(self, strValue):
@@ -32,12 +50,50 @@ class Customer:
         try:
             intValue = int(intValue)
 
-            if intValue < 0:
+            if intValue <= 0:
                 raise Exception("intID should be greater than 0")
             else:
                 self.__intID = intValue
         except ValueError:
-            raise Exception("intID should be a number")
+            raise Exception("intID should be a whole number")
+
+    @strRentalBasis.setter
+    def strRentalBasis(self, strValue):
+        if strValue == "Hourly" or strValue == "hourly":
+            self.__strRentalBasis = "Hourly"
+        elif strValue == "Daily" or strValue == "daily":
+            self.__strRentalBasis = "Daily"
+        elif strValue == "Weekly" or strValue == "weekly":
+            self.__strRentalBasis = "Weekly"
+        else:
+            raise Exception("strRentalBasis must be Hourly, Daily, or Weekly")
+
+    @fltRentalTime.setter
+    def fltRentalTime(self, fltValue):
+        try:
+            fltValue = float(fltValue)
+
+            if fltValue < 0:
+                raise Exception("fltRentalTime should be 0 or greater")
+            else:
+                self.__fltRentalTime = fltValue
+        except ValueError:
+            raise Exception("fltRentalTime should be a number")
+
+    @intRentalQuantity.setter
+    def intRentalQuantity(self, intValue):
+        try:
+            intValu = int(intValue)
+
+            if intValue < 0:
+                raise Exception("intRentalQuantity should be 0 or greater")
+            else:
+                self.__intRentalQuantity = intValue
+        except ValueError:
+            raise Exception("intRentalQuantity should be a number")
+
+
+
 
 
 # --------------------
